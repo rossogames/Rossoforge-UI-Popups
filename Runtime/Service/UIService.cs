d4d39e8ee4cd5fe7d65eaa646737b5535407b151
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-09-27 17:19:38 +0200

    avances en implementacion de UI Service

--- Runtime/Service/UIService.cs
+++ Runtime/Service/UIService.cs
@@ -1,37 +1,48 @@
+using Rossoforge.Core.Components;
 using Rossoforge.Core.Pool;
+using Rossoforge.Core.Services;
 using Rossoforge.Core.UI;
 using UnityEngine;
 
 namespace Rossoforge.UI.Service
 {
-    public class UIService: IUIService
+    public class UIService: IUIService, IInitializable
     {
         private IPoolService _poolService;
+        private GameObject _root;
 
         public UIService(IPoolService poolService)
         {
             _poolService = poolService;
         }
 
-        public T OpenPopup<T>(IPooledGameobjectData data, Vector3 position = new(), Space relativeTo = Space.Self) where T : MonoBehaviour, IPopupView
+        public void Initialize()
         {
-            //Transform parent,
-            var popupView = _poolService.Get<T>(data, null, position, relativeTo);
+            _root = new GameObject("PopupsRoot");
+            _root.AddComponent<DontDestroyRoot>();
+        }
 
+        public T OpenPopup<T>(IPooledGameobjectData data, IPopupData popupData = null, Vector3 position = new(), Space relativeTo = Space.Self) where T : MonoBehaviour, IPopupView
+        {
+            var popupView = _poolService.Get<T>(data, _root.transform, position, relativeTo);
+            return TryOpenPopup<T>(popupView, popupData);
+        }
+
+        private T TryOpenPopup<T>(T popupView, IPopupData popupData) where T : MonoBehaviour, IPopupView
+        {
             if (popupView.CanBeOpened())
             {
+                popupView.SetData(popupData);
                 popupView.Open();
                 return popupView;
             }
-            
+
             Debug.LogWarning($"Popup {popupView.name} cannot be opened. Current state: {popupView.State}");
             return null;
-
-            
         }
 
-        // CREAR ROOT DE POPUPS (CANVAS) // quizas mejor dejarlos en el root
         // OPEN WAIT UNTIL CLOSED -- 
         // POPUP CANCEL (KEYBOARD, BACK BUTTON, ETC)
+        // catchear popups activos y aumentar sorting order
     }
 }
